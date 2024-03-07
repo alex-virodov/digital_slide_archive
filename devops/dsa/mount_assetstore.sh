a7f7af7df2c16bf72703de9bc827369080370ab8
Alexandr Virodov <alexandr.vir@gmail.com>
2024-03-07 13:45:32 -0500

    Cleanup

--- devops/dsa/mount_assetstore.sh
+++
@@ -1,4 +0,0 @@
-set -x
-sudo mkdir -p /mounted_assetstore
-sudo mount -t cifs -o username=avirodov,domain=UKRDMC,iocharset=utf8,file_mode=0700,dir_mode=0700,uid=$(id -u),gid=$(id -g) //iso2smb.rdmc.org/IBI/001IBI-TCGA/DSA/assetstore /mounted_assetstore
-
